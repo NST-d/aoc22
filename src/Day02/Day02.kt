b5e03031b2f179e0bee17f4794b1c6b8c24361d0
NST-d <niklas.strahmann@tu-dortmund.de>
2022-12-02 11:42:17 +0100

    Solved day02

--- src/Day02/Day02.kt
+++ src/Day02/Day02.kt
@@ -1,5 +1,65 @@
 package Day02
 
-fun main() {
+import readInputLines
+import readTestLines
+enum class RPS(val score: Int){
+    ROCK(1),
+    PAPER(2),
+    SCISSOR (3),
+}
 
+fun winScore(opponent: RPS, player: RPS): Int {
+    if(opponent == player) return 3
+    if((opponent.score+1)%3 == player.score%3  ) return 6
+    return 0
+}
+
+fun getRPS(input:Char, base:Char) =
+    when( input-base){
+        0 -> RPS.ROCK
+        1 -> RPS.PAPER
+        2 -> RPS.SCISSOR
+        else -> error("Unssuporter input $input for base $base")
+    }
+
+
+fun main() {
+    fun part1(input : List<String>)=
+
+        input.sumOf {
+            val opponent = getRPS(it[0], 'A')
+            val recommended = getRPS(it[2], 'X')
+
+            val score =recommended.score + winScore(opponent, recommended)
+
+            println("$opponent vs $recommended -> $score")
+            score
+            }
+
+    fun part2(input: List<String>) =
+        input.sumOf {
+            val opponent = getRPS(it[0], 'A')
+
+            val recommended  = when(it[2]){
+                'X' -> RPS.values()[(opponent.score +1) % 3]
+                'Y' -> opponent
+                'Z' -> RPS.values()[(opponent.score) % 3]
+                else -> error("")
+            }
+
+            val score = recommended.score + winScore(opponent, recommended)
+
+            //println("$opponent vs $recommended -> $score")
+            score
+
+        }
+
+
+
+
+
+    val test = readTestLines("Day02")
+
+    val input = readInputLines("Day02")
+    println(part2(input))
 }
