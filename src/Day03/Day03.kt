154c6b4577866e735ffbeaa8e52daa57e467b00c
NST-d <niklas.strahmann@tu-dortmund.de>
2022-12-03 08:13:28 +0100

    Solved day03

---
+++ src/Day03/Day03.kt
@@ -0,0 +1,54 @@
+package Day03
+
+import readInputLines
+import readTestLines
+
+fun main() {
+    fun part1(input : List<String>): Int =
+        input
+            .map { it.chunked(it.length/2) }
+            .sumOf {
+                val left = it[0]
+                val right = it[1]
+
+                var value = 0
+
+                (left.toCharArray()).distinct().forEach {
+                    c: Char ->
+                    if (right.contains(c)){
+                        if(c.isUpperCase()){
+                            value += c - 'A' + 27
+                        }else{
+                            value += c - 'a' +1
+                        }
+                        //println("$c $value")
+                    }
+                }
+                value
+            }
+
+    fun part2(input: List<String>): Int =
+        input.withIndex().groupBy {  (it.index/3) }.values
+            .sumOf {
+                group ->
+                var value = 0
+
+                group[0].value.toCharArray().distinct().forEach { c: Char ->
+                    if( group[1].value.contains(c) && group[2].value.contains(c)){
+                        if(c.isUpperCase()){
+                            value += c - 'A' + 27
+                        }else{
+                            value += c - 'a' +1
+                        }
+                    }
+
+                }
+
+                value
+            }
+
+
+    val test = readTestLines("Day03")
+    val input = readInputLines("Day03")
+    println( part2(input) )
+}
