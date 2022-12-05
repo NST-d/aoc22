105b20b8d44d1fed87bdcaf1cc84c1a6b0d10041
NST-d <niklas.strahmann@tu-dortmund.de>
2022-12-05 21:36:39 +0100

    Added day05

---
+++ src/Day05/day05.kt
@@ -0,0 +1,72 @@
+package Day05
+
+import readInputString
+import readTestString
+import java.util.*
+
+fun main() {
+
+    fun parseInput(input: String): Pair<Array<Stack<Char>>, List<String>> {
+        val split = input.split("\n\n".toRegex(), limit = 2)
+        val cratesString = split[0].split("\n").toMutableList()
+        val moves = split[1].split("\n")
+
+        val amountCrates = cratesString.removeLast().split("   ").count()
+        val crates = Array(amountCrates) { Stack<Char>() }
+
+        cratesString.reversed()
+            .forEach { it ->
+                "$it ".chunked(4).forEachIndexed { j, entry ->
+                    if (entry.isNotBlank()) {
+                        crates[j].push(entry[1])
+                    }
+                }
+            }
+        return Pair(crates, moves)
+    }
+
+    fun part1(input: String): String {
+
+        val (crates, moves) = parseInput(input)
+        //println(crates.contentToString())
+
+        moves.map {
+            val words = it.split(" ")
+
+            Triple(words[1].toInt(), words[3].toInt() - 1, words[5].toInt() - 1)
+        }.forEach { (amount, src, dest) ->
+            repeat(amount) {
+                crates[dest].push(crates[src].pop())
+            }
+            //println(crates.contentToString())
+        }
+
+        return String(crates.map { it.peek() }.toCharArray())
+    }
+
+    fun part2(input: String): String {
+        val (crates, moves) = parseInput(input)
+        //println(crates.contentToString())
+        moves.map {
+            val words = it.split(" ")
+
+            Triple(words[1].toInt(), words[3].toInt() - 1, words[5].toInt() - 1)
+        }.forEach { (amount, src, dest) ->
+            val temp = List(amount) { crates[src].pop() }
+
+            temp.reversed().forEach {
+                crates[dest].push(it)
+            }
+            //println(crates.contentToString())
+        }
+        return String(crates.map { it.peek() }.toCharArray())
+    }
+
+    val test = readTestString("Day05").trimIndent()
+    val input = readInputString("Day05").trimIndent()
+
+    println(part1(input))
+    println(part2(input))
+}
+
+
